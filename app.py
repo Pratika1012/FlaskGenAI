1518ec1c23517bc0731a874bf15a9481ffa6a9d9
Pratika1012 <118338761+Pratika1012@users.noreply.github.com>
2023-10-19 10:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,129 +1,111 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from utils.dynamic_ocr import process_pdf
+from utils.single_case_summary import extract_text_from_pdf_single, chat_interface_single
+from utils.multiple_case_summary import extract_text_from_pdf_multiple, chat_interface_multiple
+from utils.translate_ocr import translate_and_process_pdf
+import os
+import openai
+import json
+from apscheduler.schedulers.background import BackgroundScheduler
+import glob
+import atexit
+import time
+from config import IP_ADDRESS, KEY_FILE, SUBDOMAIN
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/uploads')
+app.secret_key = b"gen_ai"
+
+# Ensure the 'uploads' directory exists
+upload_folder = os.path.join(app.root_path, 'static/uploads')
+os.makedirs(upload_folder, exist_ok=True)
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+def cleanup_upload_folder():
+    files = glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], "*.*"))
+    current_time = time.time()
+    
+    for file in files:
+        try:
+            file_creation_time = os.path.getctime(file)
+            if current_time - file_creation_time > 24 * 60 * 60:  # Delete files older than 24 hours
+                os.remove(file)  # Delete the file
+        except Exception as e:
+            print(f"Error deleting file: {e}")
+
+# Add the cleanup function to the scheduler to run every hour
+scheduler.add_job(cleanup_upload_folder, "interval", hours=1)
+
+def delete_uploaded_file(filename):
+    try:
+        os.remove(filename)  # Delete the uploaded file
+    except Exception as e:
+        print(f"Error deleting file: {e}")
+
+def validate_option(option):
+    valid_options = ['Dynamic OCR', 'Single Case Summary', 'Translate OCR', 'Multiple Case Summary']
+    return option in valid_options
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-import pytesseract
-import pdfplumber
-from pdf2image import convert_from_path
-import openai
-import pandas as pd
-import fitz
-
-#setting up the APIkey for OpenAI ChatGPT
-openai.api_key  = "your api key"
-
-
-#creating the function to get the response from ChatGPT API using prompts
-def get_completion(prompt, model="gpt-3.5-turbo-16k"):
-  messages = [{"role": "user", "content": prompt}]
-  response = openai.ChatCompletion.create(
-     model=model,
-     messages=messages,
+@app.route('/upload', methods=['POST'])
+def upload():
+    global filename
+    option = request.form.get('option')
     
-     temperature=0.7,
-#      temperature=0, # this is the degree of randomness of the model's output
-  )
-  return response.choices[0].message["content"]
+    # Initialize the filename variable
+    filename = None
 
-# Function to convert PDF to Images (same as before)
-def convert_pdf_to_img(pdf_file):
-    images = convert_from_path(pdf_file, poppler_path=r'C://Program Files//poppler-23.07.0//Library//bin')
-    return images
+    if 'file' in request.files:
+        file = request.files['file']
+        if file:
+            filename = os.path.join(upload_folder, secure_filename(file.filename))
+            try:
+                file.save(filename)
+                atexit.register(delete_uploaded_file, filename)
+            except Exception as e:
+                return f"Error saving file: {e}", 500
+        else:
+            return "No file uploaded", 400
 
-# Function to extract text from an Image (same as before)
-def extract_text_from_image(image):
-    text = pytesseract.image_to_string(image)
-    return text
+    api_key = request.form.get('api_key')
+    if not api_key:
+        return "API key missing", 400
 
-# Function to get text from all pages of a PDF (same as before)
-def get_text_from_any_pdf(pdf_file):
-    images = convert_pdf_to_img(pdf_file)
-    final_text = ""
-    for pg, img in enumerate(images):
-        final_text += extract_text_from_image(img)
-    return final_text
+    if not validate_option(option):
+        return "Invalid option selected", 400
 
-# Function to translate the text to English
-def translate_to_english(final_text):
-    prompt = f"""
-        UserInput Data is in spanish convert to english
-        
-        Context: {final_text}
-        
-        Answer: 
-    """
-    response = get_completion(prompt)
-    return response
+    # Update OpenAI API key
+    openai.api_key = api_key
 
-@app.route('/translate_pdf', methods=['POST'])
-def translate_pdf():
-    if 'pdf_file' not in request.files:
-        return jsonify({'error': 'No file part'})
-    file = request.files['pdf_file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
-    if file:
-        ocr_text = get_text_from_any_pdf(file)
-        translated_text = translate_to_english(ocr_text)
-        return jsonify({'translated_text': translated_text})
+    try:
+        if option == 'Dynamic OCR':
+            processed_json = process_pdf(filename)
+            formatted_dynamic_ocr = json.dumps(processed_json, indent=4)
+            return render_template('index.html', dynamic_ocr=formatted_dynamic_ocr)
 
-# Function to process the question using GPT-3.5
-def chat_with_gpt(question, pdf_text):
-    # Set up the message with user question and PDF text as context
-    messages = [{"role": "user", "content": question}, {"role": "assistant", "content": pdf_text}]
-    
-    # Get the response from GPT-3.5
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=messages,
-        temperature=0.7,
-    )
-    
-    # Extract the assistant's reply from the API response
-    assistant_reply = response['choices'][0]['message']['content']
-    return assistant_reply
+        elif option == 'Single Case Summary':
+            extracted_text = extract_text_from_pdf_single(filename)
+            chatbot_response = chat_interface_single(extracted_text)
+            return render_template('index.html', chatbot_response_single=chatbot_response)
 
-def extract_text_from_pdf(pdf_file):
-    text = ""
-    with fitz.open(stream=pdf_file.read(), filetype="pdf") as pdf_document:
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document.load_page(page_num)
-            text += page.get_text()
-    return text
+        elif option == 'Translate OCR':
+            translated_json = translate_and_process_pdf(filename)
+            formatted_translated_json = json.dumps(translated_json, indent=4)
+            return render_template('index.html', translated_ocr=formatted_translated_json)
 
-
-@app.route('/chat_gpt', methods=['POST'])
-def chat_gpt():
-    if 'pdf_file' not in request.files:
-        return jsonify({'error': 'No file part'})
-    pdf_file = request.files['pdf_file']
-    if pdf_file.filename == '':
-        return jsonify({'error': 'No selected file'})
-    question = request.form['question']
-    if not question:
-        return jsonify({'error': 'No question provided'})
-    if pdf_file:
-        pdf_text = extract_text_from_pdf(pdf_file)
-        response = chat_with_gpt(question, pdf_text)
-        return jsonify({'response': response})
-    
-
-@app.route('/data_processing', methods=['POST'])
-def data_processing():
-    # Implement data processing logic here
-    # For simplicity, let's assume it returns a placeholder response
-    return jsonify({'response': 'Data processing is in progress.'})
-
+        elif option == 'Multiple Case Summary':
+            extracted_text = extract_text_from_pdf_multiple(filename)
+            chatbot_response = chat_interface_multiple(extracted_text)
+            return render_template('index.html', chatbot_response_multiple=chatbot_response)
+    except Exception as e:
+        return f"An error occurred: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug = True)
-
-
-
-
-# <link rel="stylesheet" href="{{ url_for('static', filename='style.css') }}">
-# <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    app.run(debug=True)
